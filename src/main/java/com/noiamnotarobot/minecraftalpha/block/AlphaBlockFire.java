0ceda85ec499b8665f37bc45bbcb0be2f646f13b
NoIAmNotARobot <237332689+NoIAmNotARobot@users.noreply.github.com>
2025-10-25 18:10:32 -0400

    Fixed fire, yet to fix model

--- src/main/java/com/noiamnotarobot/minecraftalpha/block/AlphaBlockFire.java
+++ src/main/java/com/noiamnotarobot/minecraftalpha/block/AlphaBlockFire.java
@@ -1,54 +1,86 @@
 package com.noiamnotarobot.minecraftalpha.block;
 
 import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.Random;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockFire;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class AlphaBlockFire extends AlphaBlock {
+import com.google.common.collect.Maps;
+import com.noiamnotarobot.minecraftalpha.MinecraftAlpha;
 
-    private final HashMap<Block, Integer> chanceToEncourageFire = new HashMap<>();
-    private final HashMap<Block, Integer> abilityToCatchFire = new HashMap<>();
+public class AlphaBlockFire extends BlockFire {
+
+    private final HashMap<Integer, Integer> encouragements = new HashMap<>();
+    private final HashMap<Integer, Integer> flammabilities = new HashMap<>();
+    protected String unlocalizedName = "fire";
 
     protected AlphaBlockFire() {
-        super(Material.fire, "fire", false);
-        this.initializeBlock(AlphaBlock.planks, 5, 20);
-        this.initializeBlock(AlphaBlock.wood, 5, 5);
-        this.initializeBlock(AlphaBlock.leaves, 30, 60);
-        this.initializeBlock(AlphaBlock.bookshelf, 30, 20);
-        this.initializeBlock(AlphaBlock.tnt, 15, 100);
-        this.initializeBlock(AlphaBlock.cloth, 30, 60);
-        this.setTickRandomly(true);
-    }
-
-    private void initializeBlock(Block var1, int var2, int var3) {
-        this.chanceToEncourageFire.put(var1, var2);
-        this.abilityToCatchFire.put(var1, var3);
-    }
-
-    public AxisAlignedBB getCollisionBoundingBoxFromPool(World var1, int var2, int var3, int var4) {
-        return null;
-    }
-
-    public boolean isOpaqueCube() {
-        return false;
-    }
-
-    public boolean renderAsNormalBlock() {
-        return false;
-    }
-
-    public int getRenderType() {
-        return 3;
+        super();
+        this.setBlockName(unlocalizedName);
+        this.setBlockTextureName(MinecraftAlpha.MODID + ":" + unlocalizedName);
+        this.setFireInfo(AlphaBlock.planks, 5, 20);
+        this.setFireInfo(AlphaBlock.wood, 5, 5);
+        this.setFireInfo(AlphaBlock.leaves, 30, 60);
+        this.setFireInfo(AlphaBlock.bookshelf, 30, 20);
+        this.setFireInfo(AlphaBlock.tnt, 15, 100);
+        this.setFireInfo(AlphaBlock.cloth, 30, 60);
     }
 
     public int quantityDropped(int var1, int var2, Random var3) {
         return 0;
+    }
+
+    private static class FireInfo {
+
+        private int encouragement = 0;
+        private int flammibility = 0;
+    }
+
+    private final IdentityHashMap<Block, FireInfo> blockInfo = Maps.newIdentityHashMap();
+
+    public void setFireInfo(Block block, int encouragement, int flammibility) {
+        if (block == Blocks.air) throw new IllegalArgumentException("Tried to set air on fire... This is bad.");
+        int id = Block.getIdFromBlock(block);
+        this.encouragements.put(id, encouragement);
+        this.flammabilities.put(id, flammibility);
+
+        FireInfo info = getInfo(block);
+        info.encouragement = encouragement;
+        info.flammibility = flammibility;
+    }
+
+    private FireInfo getInfo(Block block) {
+        FireInfo ret = blockInfo.get(block);
+        if (ret == null) {
+            ret = new FireInfo();
+            blockInfo.put(block, ret);
+        }
+        return ret;
+    }
+
+    public void rebuildFireInfo() {
+        encouragements.clear();
+        flammabilities.clear();
+
+        for (Map.Entry<Block, FireInfo> e : blockInfo.entrySet()) {
+            int id = Block.getIdFromBlock(e.getKey());
+            encouragements.put(id, e.getValue().encouragement);
+            flammabilities.put(id, e.getValue().flammibility);
+        }
+    }
+
+    public int getFlammability(Block block) {
+        return flammabilities.getOrDefault(Block.getIdFromBlock(block), 0);
+    }
+
+    public int getEncouragement(Block block) {
+        return encouragements.getOrDefault(Block.getIdFromBlock(block), 0);
     }
 
     public void updateTick(World var1, int var2, int var3, int var4, Random var5) {
@@ -97,7 +129,7 @@ public class AlphaBlockFire extends AlphaBlock {
     }
 
     private void tryToCatchBlockOnFire(World var1, int var2, int var3, int var4, int var5, Random var6) {
-        int var7 = this.abilityToCatchFire.getOrDefault(var1.getBlock(var2, var3, var4), 0);
+        int var7 = this.getFlammability(var1.getBlock(var2, var3, var4));
         if (var6.nextInt(var5) < var7) {
             boolean var8 = var1.getBlock(var2, var3, var4) == AlphaBlock.tnt;
             if (var6.nextInt(2) == 0) {
@@ -136,17 +168,12 @@ public class AlphaBlockFire extends AlphaBlock {
         }
     }
 
-    public boolean isCollidable() {
-        return false;
-    }
-
     public boolean canBlockCatchFire(IBlockAccess var1, int var2, int var3, int var4) {
-        return this.chanceToEncourageFire.getOrDefault(var1.getBlock(var2, var3, var4), 0) > 0;
+        return this.getEncouragement(var1.getBlock(var2, var3, var4)) > 0;
     }
 
     public int getChanceToEncourageFire(World var1, int var2, int var3, int var4, int var5) {
-        int var6 = this.chanceToEncourageFire.getOrDefault(var1.getBlock(var2, var3, var4), 0);
-        return Math.max(var6, var5);
+        return Math.max(this.getEncouragement(var1.getBlock(var2, var3, var4)), var5);
     }
 
     public boolean canPlaceBlockAt(World var1, int var2, int var3, int var4) {
@@ -240,5 +267,9 @@ public class AlphaBlockFire extends AlphaBlock {
             }
         }
 
+    }
+
+    public String getUnlocalizedName() {
+        return "tile." + MinecraftAlpha.MODID + "." + unlocalizedName;
     }
 }
